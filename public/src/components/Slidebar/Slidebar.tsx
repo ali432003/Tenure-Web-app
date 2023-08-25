53526cac5f15238ab17c8f249e32246a88227772
Ali Aamir <111776866+ali432003@users.noreply.github.com>
2023-08-25 17:12:15 +0500

    new file:   public/src/assets/icons/amt_saved.svg 	new file:   public/src/assets/icons/arrow_up.svg 	new file:   public/src/assets/icons/cash_back.svg 	new file:   public/src/components/Box/Box.tsx 	new file:   public/src/components/Slidebar/Slidebar.tsx 	modified:   public/src/pages/Dashboard/Empty.tsx

---
+++ public/src/components/Slidebar/Slidebar.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function Slidebar() {
+    return (
+        <div className='w-1/6 h-min-screen' style={{ backgroundColor: "rgb(56 133 123 /1)", color: 'white' }}>
+            {/* slide bar */}
+            <div className='mt-5 text-center'>
+                <a href="" className='text-white font-semibold text-center text-lg p-3 transition-colors flex items-center ms-4'>
+                    <img src="src/assets/icons/logo_icon_small_1.svg" className='mr-1' alt="" />
+                    <h1 >Tenure</h1>
+                </a>
+            </div>
+            <div style={{ marginTop: '50%' }}>
+                <div className='pb-1'>
+                    <a href="" className='text-white font-semibold text-center text-lg p-3 transition-colors flex items-center ms-4'>
+                        <img src="src/assets/icons/logo_icon_small_1.svg" className='mr-1' alt="" />
+                        Home
+                    </a>
+                </div>
+                <div className='pb-1'>
+                    <a href="" className='text-white font-semibold text-center text-lg p-3 transition-colors flex items-center ms-4'>
+                        <img src="src/assets/icons/employees_2.svg" className='mr-5' alt="" />Employee</a>
+                </div>
+                <div className='pb-1'>
+                    <a href="" className='text-white font-semibold text-center text-lg p-3 transition-colors flex items-center ms-4'>
+                        <img src="src/assets/icons/deals_2.svg" className='mr-4' alt="" />Deals</a>
+                </div>
+                <div className='pb-1'>
+                    <a href="" className='text-white font-semibold text-center text-lg p-3 transition-colors flex items-center ms-4'>
+                        <img src="src/assets/icons/wallet_2.svg" className='mr-4' alt="" />Wallet</a>
+                </div>
+                <div className='pb-1'>
+                    <a href="" className='text-white font-semibold text-center text-lg p-3 transition-colors flex items-center ms-4'>
+                        <img src="src/assets/icons/settings_2.svg" className='mr-4' alt="" />Settings</a>
+                </div>
+                <div className='pb-1'>
+                    <a href="" className='text-white font-semibold text-center text-lg p-3 transition-colors flex items-center ms-4'>
+                        <img src="src/assets/icons/logout_2.svg" className='mr-4' alt="" />Logout</a>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Slidebar;
--- public/src/pages/Dashboard/Empty.tsx
+++ public/src/pages/Dashboard/Empty.tsx
@@ -1,61 +1,33 @@
-import React from 'react'
+import React from 'react';
 import './Emty.css';
 import Button from '../../components/Button/Button';
+import Slidebar from '../../components/Slidebar/Slidebar';
+import Box from '../../components/Box/Box';
+
 
 
 function Empty() {
-
     return (
-        <div className='flex justify-self-center'>
-            <div className='w-1/6  min-h-screen' style={{ backgroundColor: "rgb(56 133 123 /1)", color: 'white' }}>
-                {/* slide bar */}
-                <div className='mt-5 text-center'>
-                    <img src="src/assets/icons/logo_icon_small_1.svg" className='ms-4' alt="" style={{ position: 'absolute' }} />
-                    <h1 >Tenure</h1>
-                </div>
-                <div style={{ marginTop: '50%' }}>
-                    <div className='pb-8'>
-                        <a href="" className='text-white font-semibold text-center text-lg p-3 transition-colors'>
-                            <img src="src/assets/icons/logo_icon_small_1.svg" width={'2%'} className='ms-10' style={{ position: 'absolute' }} alt="" />Home
-                        </a>
-                    </div>
-                    <div className='pb-8'>
-                        <a href="" className='text-white font-semibold text-center text-lg p-3 transition-colors ms-9'>
-                            <img src="src/assets/icons/employees_2.svg" width={'2%'} className='ms-10' style={{ position: 'absolute' }} alt="" />Employee</a>
-                    </div>
-                    <div className='pb-8'>
-                        <a href="" className='text-white font-semibold text-center text-lg p-3 transition-colors '>
-                            <img src="src/assets/icons/deals_2.svg" width={'2%'} className='ms-10' style={{ position: 'absolute' }} alt="" />Deals</a>
-                    </div>
-                    <div className='pb-8'>
-                        <a href="" className='text-white font-semibold text-center text-lg p-3 transition-colors '>
-                            <img src="src/assets/icons/wallet_2.svg" width={'2%'} className='ms-10' style={{ position: 'absolute' }} alt="" />Wallet</a>
-                    </div>
-                    <div className='pb-8'>
-                        <a href="" className='text-white font-semibold text-center text-lg p-3 transition-colors ms-5'>
-                            <img src="src/assets/icons/settings_2.svg" width={'2%'} className='ms-10' style={{ position: 'absolute' }} alt="" />Settings</a>
-                    </div>
-                    <div className='pb-8'>
-                        <a href="" className='text-white font-semibold text-center text-lg p-3 transition-colors '>
-                            <img src="src/assets/icons/logout_2.svg" width={'2%'} className='ms-10' style={{ position: 'absolute' }} alt="" />Logout</a>
-                    </div>
-                </div>
-            </div>
-            <div className='w-5/6 min-h-screen'>
+        <div className='flex h-min-screen'>
+            <Slidebar />
+            <div className='w-5/6'>
                 {/* main div */}
-
-                <div className='text-start ms-5 mt-5'>
+                <div className='flex items-center justify-between text-start ms-5 mt-5'>
                     <h1>Welcome, Jon</h1>
                 </div>
                 <div className='mt-5'>
-                    <div className='me-11'>
-                        <button style={{ position: 'absolute', right: '50%' }} className='border-2 border-solid rounded-lg p-2 -mt-2'>
-                            <img src="src/assets/icons/chevron_left.svg" alt="" />
-                        </button>
+                    <div>
+                        <div dir='rtl'>
+                            <button style={{ position: 'absolute' }} className='border-2 border-solid rounded-lg p-2 -mt-2 ms-11'>
+                                <img src="src/assets/icons/chevron_left.svg" alt="" />
+                            </button>
+                        </div>
                         <h4>June, 2023</h4>
-                        <button style={{ position: 'absolute', left: '62%' }} className='border-2 border-solid rounded-lg p-2 -mt-8'>
-                            <img src="src/assets/icons/chevron_right.svg" alt="" />
-                        </button>
+                        <div dir='ltr'>
+                            <button style={{ position: 'absolute' }} className='border-2 border-solid rounded-lg p-2 -mt-8 ms-11'>
+                                <img src="src/assets/icons/chevron_right.svg" alt="" />
+                            </button>
+                        </div>
                     </div>
                     <div>
                         <ul className='flex justify-end -mt-8'>
@@ -66,13 +38,14 @@ function Empty() {
                         </ul>
                     </div>
                 </div>
-                <div className='mt-5 ms-5 grid grid-rows-2 grid-flow-col gap-4'>
-                    <div className='bg-yellow-400 rounded-lg' style={{ padding: '25%' }}></div>
-                    <div className='bg-blue-400 rounded-lg' style={{ padding: '25%' }}></div>
-                    <div className='bg-green-400 rounded-lg' style={{ padding: '25%' }}></div>
-                    <div className='bg-red-400 rounded-lg' style={{ padding: '25%' }}></div>
+                <div className='mt-5 ms-5 grid grid-cols-2 gap-4'>
+                    <Box title="Employee contribution" img="gift.svg" valueT = "$1,520" valueL = "$1,520" avg="contribution" h6='Receivers'/>
+                    <Box title="Total amount saved" img="amt_saved.svg" valueT = "$1,520" valueL = "$1,520" avg='savings' h6='Total no of savers'/>
+                    <Box title="Total round-ups value" img="arrow_up.svg" valueT = "$1,520" valueL = "$1,520" avg='round-ups' h6='Total no of transection'/>
+                    <Box title="Total cash-back value" img="cash_back.svg" valueT = "$1,520" valueL = "$1,520" avg="cash-back" h6='Total no of transection'/>
                 </div>
                 <div className='mt-5 ms-5'>
+                    {/* ... your active offers section ... */}
                     <h4 className='text-start'>
                         Active offers
                     </h4>
@@ -80,10 +53,9 @@ function Empty() {
                         <p style={{ color: 'grey', padding: '5%' }}>There is no active offers at this time</p>
                     </div>
                 </div>
-
             </div>
         </div>
-    )
+    );
 }
 
-export default Empty
+export default Empty;
