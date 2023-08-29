8f4dee296402439f6ae23af838a4381701699aeb
Ali Aamir <111776866+ali432003@users.noreply.github.com>
2023-08-29 14:39:42 +0500

    modified:   public/src/pages/Employees/Employees.tsx 	modified:   public/src/components/Em/EmTableRows.tsx

--- public/src/pages/Employees/Employees.tsx
+++ public/src/pages/Employees/Employees.tsx
@@ -42,7 +42,7 @@ export default function Employees() {
         // renders the default empty page made for employees
         let di = document.getElementById('di')
         if (di) {
-            di.style.display = 'flex';
+            di.style.display = 'none';
         }
     }
     const dataOfPurch = [
@@ -61,6 +61,19 @@ export default function Employees() {
         },
         {
             id: 2,
+            name: "McDonald's",
+            category: 'food',
+            img: 'mc.svg',
+            purch: '500',
+            deals: '3',
+            rupValue: '$300',
+            chBack: '$400',
+            catimg: 'food.svg',
+            catCol: '#DA6252',
+            cat: 'Food',
+        },
+        {
+            id: 3,
             name: 'Healthfit',
             category: 'Health-care',
             img: 'health_fit.svg',
@@ -73,7 +86,59 @@ export default function Employees() {
             cat: 'Health-care',
         },
         {
-            id: 3,
+            id: 4,
+            name: 'Healthfit',
+            category: 'Health-care',
+            img: 'health_fit.svg',
+            purch: '500',
+            deals: '3',
+            rupValue: '$300',
+            chBack: '$400',
+            catimg: 'healthcare.svg',
+            catCol: '#972144',
+            cat: 'Health-care',
+        },
+        {
+            id: 5,
+            name: 'Healthfit',
+            category: 'Health-care',
+            img: 'health_fit.svg',
+            purch: '500',
+            deals: '3',
+            rupValue: '$300',
+            chBack: '$400',
+            catimg: 'healthcare.svg',
+            catCol: '#972144',
+            cat: 'Health-care',
+        },
+        {
+            id: 6,
+            name: 'Healthfit',
+            category: 'Health-care',
+            img: 'health_fit.svg',
+            purch: '500',
+            deals: '3',
+            rupValue: '$300',
+            chBack: '$400',
+            catimg: 'healthcare.svg',
+            catCol: '#972144',
+            cat: 'Health-care',
+        },
+        {
+            id: 7,
+            name: 'Healthfit',
+            category: 'Health-care',
+            img: 'health_fit.svg',
+            purch: '500',
+            deals: '3',
+            rupValue: '$300',
+            chBack: '$400',
+            catimg: 'healthcare.svg',
+            catCol: '#972144',
+            cat: 'Health-care',
+        },
+        {
+            id: 8,
             name: 'MakeMyTrip',
             category: 'travel',
             img: 'make_my_trip.svg',
@@ -86,7 +151,59 @@ export default function Employees() {
             cat: 'Travel',
         },
         {
-            id: 4,
+            id: 9,
+            name: 'MakeMyTrip',
+            category: 'travel',
+            img: 'make_my_trip.svg',
+            purch: '500',
+            deals: '3',
+            rupValue: '$300',
+            chBack: '$400',
+            catimg: 'travel.svg',
+            catCol: '#B77900',
+            cat: 'Travel',
+        },
+        {
+            id: 10,
+            name: 'MakeMyTrip',
+            category: 'travel',
+            img: 'make_my_trip.svg',
+            purch: '500',
+            deals: '3',
+            rupValue: '$300',
+            chBack: '$400',
+            catimg: 'travel.svg',
+            catCol: '#B77900',
+            cat: 'Travel',
+        },
+        {
+            id: 11,
+            name: 'MakeMyTrip',
+            category: 'travel',
+            img: 'make_my_trip.svg',
+            purch: '500',
+            deals: '3',
+            rupValue: '$300',
+            chBack: '$400',
+            catimg: 'travel.svg',
+            catCol: '#B77900',
+            cat: 'Travel',
+        },
+        {
+            id: 12,
+            name: 'MakeMyTrip',
+            category: 'travel',
+            img: 'make_my_trip.svg',
+            purch: '500',
+            deals: '3',
+            rupValue: '$300',
+            chBack: '$400',
+            catimg: 'travel.svg',
+            catCol: '#B77900',
+            cat: 'Travel',
+        },
+        {
+            id: 13,
             name: 'Starbucks',
             category: 'food',
             img: 'starbucks.svg',
@@ -99,7 +216,20 @@ export default function Employees() {
             cat: 'Food',
         },
         {
-            id: 5,
+            id: 14,
+            name: 'Starbucks',
+            category: 'food',
+            img: 'starbucks.svg',
+            purch: '500',
+            deals: '3',
+            rupValue: '$300',
+            chBack: '$400',
+            catimg: 'food.svg',
+            catCol: '#DA6252',
+            cat: 'Food',
+        },
+        {
+            id: 15,
             name: 'GAP',
             category: 'retail',
             img: 'gap.svg',
@@ -111,8 +241,68 @@ export default function Employees() {
             catCol: '#004E5F',
             cat: 'Retail',
         },
-        
+        {
+            id: 16,
+            name: 'GAP',
+            category: 'retail',
+            img: 'gap.svg',
+            purch: '500',
+            deals: '3',
+            rupValue: '$300',
+            chBack: '$400',
+            catimg: 'retail.svg',
+            catCol: '#004E5F',
+            cat: 'Retail',
+        },
+        {
+            id: 17,
+            name: 'GAP',
+            category: 'retail',
+            img: 'gap.svg',
+            purch: '500',
+            deals: '3',
+            rupValue: '$300',
+            chBack: '$400',
+            catimg: 'retail.svg',
+            catCol: '#004E5F',
+            cat: 'Retail',
+        },
+        {
+            id: 18,
+            name: 'GAP',
+            category: 'retail',
+            img: 'gap.svg',
+            purch: '500',
+            deals: '3',
+            rupValue: '$300',
+            chBack: '$400',
+            catimg: 'retail.svg',
+            catCol: '#004E5F',
+            cat: 'Retail',
+        },
+
     ];
+    const dataOfMain = [
+        { img: "jon.svg", name: "Jon Snow", goal: "New car, First home, Vacation, Engagament ring" },
+        { img: "vinn.svg", name: "Vinn Isuis", goal: "Vacation" },
+        { img: "rodd.svg", name: "Rodd Rigo", goal: "Mortgage, Birthday shopping" },
+        { img: "sam.svg", name: "Sam Anther", goal: "Rent, New car, New TV" },
+        { img: "eden.svg", name: "Eden Garden", goal: "Family vacation, Greece trip" },
+        { img: "gabi.svg", name: "Gabi Cunha", goal: "Birthday party" },
+        { img: "ronald.svg", name: "Ronald Christian", goal: "New car, Vacation, Christmas shopping" },
+        { img: "lionel.svg", name: "Lionel Richmond", goal: "Summer party" }
+    ];
+    const dataOfRecie = [
+        { img: "jon.svg", name: "Jon Snow", goal: "$1,340.56" },
+        { img: "vinn.svg", name: "Vinn Isuis", goal: "$1,200" },
+        { img: "rodd.svg", name: "Rodd Rigo", goal: "$1,000" },
+        { img: "sam.svg", name: "Sam Anther", goal: "$800" },
+        { img: "eden.svg", name: "Eden Garden", goal: "$750" },
+        { img: "gabi.svg", name: "Gabi Cunha", goal: "$600" },
+        { img: "ronald.svg", name: "Ronald Christian", goal: "$560" },
+        { img: "lionel.svg", name: "Lionel Richmond", goal: "$400" }
+    ];
+
 
     const filterByCategory = (category: any) => {
         setSelectedCategory(category);
@@ -146,14 +336,18 @@ export default function Employees() {
             }
             return (
                 <>
-                    <EmTableRows rkey={currentKey} img="jon.svg" name="Jon Snow" goal="New car, First home, Vacation, Engagament ring" />
-                    <EmTableRows rkey={currentKey} img="vinn.svg" name="Vinn Isuis" goal="Vacation" />
-                    <EmTableRows rkey={currentKey} img="rodd.svg" name="Rodd Rigo" goal="Mortgage, Birthday shopping" />
-                    <EmTableRows rkey={currentKey} img="sam.svg" name="Sam Anther" goal="Rent, New car, New TV" />
-                    <EmTableRows rkey={currentKey} img="eden.svg" name="Eden Garden" goal="Family vacation, Greece trip" />
-                    <EmTableRows rkey={currentKey} img="gabi.svg" name="Gabi Cunha" goal="Birthday party" />
-                    <EmTableRows rkey={currentKey} img="ronald.svg" name="Ronald Christian" goal="New car, Vacation, Christmas shopping" />
-                    <EmTableRows rkey={currentKey} img="lionel.svg" name="Lionel Richmond" goal="Summer party" />
+                    {
+                        dataOfMain.map((item, index) => (
+                            <EmTableRows
+                                rkey={currentKey}
+                                key={index}
+                                img={item.img}
+                                name={item.name}
+                                // progress = {item.prog} for overall progress
+                                goal={item.goal}
+                            />
+                        ))}
+
                 </>
             );
         } else if (currentKey === 'receiver') {
@@ -167,14 +361,17 @@ export default function Employees() {
             }
             return (
                 <>
-                    < EmTableRows rkey={currentKey} img="jon.svg" name="Jon Snow" goal="$1,340.56" />
-                    < EmTableRows rkey={currentKey} img="vinn.svg" name="Vinn Isuis" goal="$1,200" />
-                    < EmTableRows rkey={currentKey} img="rodd.svg" name="Rodd Rigo" goal="$1,000" />
-                    < EmTableRows rkey={currentKey} img="sam.svg" name="Sam Anther" goal="$800" />
-                    < EmTableRows rkey={currentKey} img="eden.svg" name="Eden Garden" goal="$750" />
-                    < EmTableRows rkey={currentKey} img="gabi.svg" name="Gabi Cunha" goal="$600" />
-                    < EmTableRows rkey={currentKey} img="ronald.svg" name="Ronald Christian" goal="$560" />
-                    < EmTableRows rkey={currentKey} img="lionel.svg" name="Lionel Richmond" goal="$400" />
+                    {
+                        dataOfRecie.map((item, index) => (
+                            <EmTableRows
+                                rkey={currentKey}
+                                key={index}
+                                img={item.img}
+                                name={item.name}
+                                // progress = {item.prog} for overall progress
+                                goal={item.goal}
+                            />
+                        ))}
                 </>
             );
         } else if (currentKey === 'purchase') {
@@ -204,63 +401,12 @@ export default function Employees() {
                                 catCol={item.catCol}
                                 cat={item.cat}
                             />
-                            <EmTableRows
-                                rkey={currentKey}
-                                selectedCategory={selectedCategory}
-                                img={item.img}
-                                name={item.name}
-                                purch={item.purch}
-                                deals={item.deals}
-                                rupValue={item.rupValue}
-                                chBack={item.chBack}
-                                catimg={item.catimg}
-                                catCol={item.catCol}
-                                cat={item.cat}
-                            />
-                            <EmTableRows
-                                rkey={currentKey}
-                                selectedCategory={selectedCategory}
-                                img={item.img}
-                                name={item.name}
-                                purch={item.purch}
-                                deals={item.deals}
-                                rupValue={item.rupValue}
-                                chBack={item.chBack}
-                                catimg={item.catimg}
-                                catCol={item.catCol}
-                                cat={item.cat}
-                            />
-                            <EmTableRows
-                                rkey={currentKey}
-                                selectedCategory={selectedCategory}
-                                img={item.img}
-                                name={item.name}
-                                purch={item.purch}
-                                deals={item.deals}
-                                rupValue={item.rupValue}
-                                chBack={item.chBack}
-                                catimg={item.catimg}
-                                catCol={item.catCol}
-                                cat={item.cat}
-                            />
-                            <EmTableRows
-                                rkey={currentKey}
-                                selectedCategory={selectedCategory}
-                                img={item.img}
-                                name={item.name}
-                                purch={item.purch}
-                                deals={item.deals}
-                                rupValue={item.rupValue}
-                                chBack={item.chBack}
-                                catimg={item.catimg}
-                                catCol={item.catCol}
-                                cat={item.cat}
-                            />
+
                         </React.Fragment>
                     ))}
                 </>
             );
-            
+
         } else {
             //  Default case when no key is selected
             // return null;
